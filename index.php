377ee295be6d7a7c1127dc41f33055cff0d7eec9
Oluwatobi <tellajames63@gmail.com>
2023-04-26 22:08:31 +0100

    CarRental

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ *   
+ *   
+ * Welcome
+ *   
+ *
+
+ */
+
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+    $uri = 'https://';
+} else {
+    $uri = 'http://';
+}
+$uri .= $_SERVER['HTTP_HOST'];
+/* Redirect To Index Under Views */
+header('Location: ' . $uri . '/CarRentalMS/ui/');
+exit;
